78dd9f0969e024f05f8765d2df415811dad01af7
captam3rica <jmattwilson1@protonmail.com>
2020-04-26 17:21:56 -0400

    Formatting update on the YOUR_ORG_NAME_HERE var

--- depNotify.sh
+++ depNotify.sh
@@ -53,7 +53,7 @@
   BANNER_IMAGE_PATH="/Applications/Self Service.app/Contents/Resources/AppIcon.icns"
 
 # Update the variable below replacing "Organization" with the actual name of your organization. Example "ACME Corp Inc."
-YOUR_ORG_NAME_HERE="Organization"
+  YOUR_ORG_NAME_HERE="Organization"
 
 # Main heading that will be displayed under the image
 # If this variable is left blank, the generic banner will appear
